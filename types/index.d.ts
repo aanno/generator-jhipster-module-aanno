a836ec4a5fccee78fcc7ee2273766341adfff7b5
aanno <aanno@users.noreply.github.com>
2020-05-08 18:15:23 +0200

    eslint fixes

--- types/index.d.ts
+++ types/index.d.ts
@@ -26,34 +26,59 @@ interface IPromptAnswers {
 
 export class CBaseGenerator implements IOptions, ILogging, IJHipsterAppConfig {
     message: string
+
     jhipsterAppConfig: IJHipsterAppConfig
+
     baseName: string
+
     packageName: string
+
     packageFolder: string
+
     clientFramework: any
+
     clientPackageManager: any
+
     angularAppName: string
+
     jhipsterVersion: string
+
     buildTool: any
+
     template: any
+
     options: Map<string, any>
+
     promptAnswers: IPromptAnswers
+
     entityConfig: any
+
     yourOptionKey: string
+
     env: ILogging
 
     getAllJhipsterConfig: () => any
+
     getAngularAppName: () => string
+
     getAngularXAppName: () => string
+
     printJHipsterLogo: () => void
+
     async: () => any
+
     prompt: ([]) => Promise<any>
+
     registerModule: (...module: string[]) => void
+
     installDependencies: (object) => void
+
     updateEntityConfig: (filename: string, key: string, value: string) => void
 
     log: (msg: string) => void
+
     error: (msg: string) => void
+
     warning: (msg: string) => void
 }
 
