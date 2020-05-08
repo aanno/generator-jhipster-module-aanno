44b1bcb44e5c79fd1ca9055f1a06b448236ab923
aanno <aanno@users.noreply.github.com>
2020-05-08 11:19:24 +0200

    format fixes I

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -4,45 +4,45 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('JHipster generator aanno', () => {
-    describe('Test with Maven and AngularX', () => {
-        beforeEach(done => {
-            helpers
-                .run(path.join(__dirname, '../generators/app'))
-                .inTmpDir(dir => {
-                    fse.copySync(path.join(__dirname, '../test/templates/maven-angularX'), dir);
-                })
-                .withOptions({
-                    testmode: true
-                })
-                .withPrompts({
-                    message: 'simple message to say hello'
-                })
-                .on('end', done);
-        });
-
-        it('generate dummy.txt file', () => {
-            assert.file(['dummy-maven.txt', 'dummy-angularX.txt']);
-        });
+  describe('Test with Maven and AngularX', () => {
+    beforeEach(done => {
+      helpers
+        .run(path.join(__dirname, '../generators/app'))
+        .inTmpDir(dir => {
+          fse.copySync(path.join(__dirname, '../test/templates/maven-angularX'), dir);
+        })
+        .withOptions({
+          testmode: true
+        })
+        .withPrompts({
+          message: 'simple message to say hello'
+        })
+        .on('end', done);
     });
 
-    describe('Test with Gradle and React', () => {
-        beforeEach(done => {
-            helpers
-                .run(path.join(__dirname, '../generators/app'))
-                .inTmpDir(dir => {
-                    fse.copySync(path.join(__dirname, '../test/templates/gradle-react'), dir);
-                })
-                .withOptions({
-                    testmode: true
-                })
-                .withPrompts({
-                    message: 'simple message to say hello'
-                })
-                .on('end', done);
-        });
-
-        it('generate dummy.txt file', () => {
-            assert.file(['dummy-gradle.txt', 'dummy-react.txt']);
-        });
+    it('generate dummy.txt file', () => {
+      assert.file(['dummy-maven.txt', 'dummy-angularX.txt']);
     });
+  });
+
+  describe('Test with Gradle and React', () => {
+    beforeEach(done => {
+      helpers
+        .run(path.join(__dirname, '../generators/app'))
+        .inTmpDir(dir => {
+          fse.copySync(path.join(__dirname, '../test/templates/gradle-react'), dir);
+        })
+        .withOptions({
+          testmode: true
+        })
+        .withPrompts({
+          message: 'simple message to say hello'
+        })
+        .on('end', done);
+    });
+
+    it('generate dummy.txt file', () => {
+      assert.file(['dummy-gradle.txt', 'dummy-react.txt']);
+    });
+  });
 });
